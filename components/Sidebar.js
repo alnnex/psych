d5c29f95dad4e72f5d2b1943e4607528692b4503
alnnex <alnnexquiwag123@gmail.com>
2022-10-12 10:44:49 +0800

    fixes

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,32 +1,9 @@
 import React from "react";
-import { Modal, Accordion, Carousel } from "flowbite-react";
-export default function Sidebar() {
+
+export default function () {
   return (
     <>
-      <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
-        <Carousel>
-          <img
-            src="https://flowbite.com/docs/images/carousel/carousel-1.svg"
-            alt="..."
-          />
-          <img
-            src="https://flowbite.com/docs/images/carousel/carousel-2.svg"
-            alt="..."
-          />
-          <img
-            src="https://flowbite.com/docs/images/carousel/carousel-3.svg"
-            alt="..."
-          />
-          <img
-            src="https://flowbite.com/docs/images/carousel/carousel-4.svg"
-            alt="..."
-          />
-          <img
-            src="https://flowbite.com/docs/images/carousel/carousel-5.svg"
-            alt="..."
-          />
-        </Carousel>
-      </div>
+      SideBar
     </>
   );
 }
